a6237d1ce85076b31fe446da8d5f27d75683bb92
Malvikagaur <malvikagaurmg@gmail.com>
2025-02-20 11:17:18 +0530

    all errors removed

--- ROUTES/userRoutes.js
+++ ROUTES/userRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { getUserProfile, updateUserProfile, updateUser } = require('../controllers/userController');
 const authMiddleware = require('../MIDDLEWARE/authMiddleware');
-const adminMiddleware = require('../middleware/adminMiddleware'); // Ensure admin check
+//const adminMiddleware = require('../middleware/adminMiddleware'); // Ensure admin check
 
 const router = express.Router();
 
 router.get('/profile', authMiddleware, getUserProfile);
 router.put('/profile', authMiddleware, updateUserProfile);
-router.put('/:id', authMiddleware, adminMiddleware, updateUser); // Admin-only route
+router.put('/:id', authMiddleware, updateUser); // Admin-only route
 
 module.exports = router;
 
